1ca3166d465b17274dc8e2a4d45a01658a79b5a2
Ostrich <31010112+SomeDumbOstrich@users.noreply.github.com>
2017-10-18 21:21:04 -0400

    Add files via upload

--- 'strichling.js
+++ 'strichling.js
@@ -3,6 +3,7 @@ var token = "MzY1NjIzNjI1NDI1ODc5MDUx.DLhNVw.f2enXsWGI5J5Y92frX7ug2QtJBg";
 var call = "~"
 var client = new Discord.Client();
 client.on("message",msg =>{
+args = msg.content.split(" ");
 if (msg.content==call+"help")
 {
 msg.channel.send("nyaaaaaa~")
@@ -11,9 +12,9 @@ if (msg.content==call+"destroy-earth")
 {
 msg.channel.send(":earth_americas::boom:")
 }
-if (msg.content==call+"test")
+if (args[0]==call+"roulette")
 {
-switch(Math.round(Math.random()*3))
+switch(Math.round(Math.random()*args[1]))
 {
 case 1:
 msg.channel.send("test1");
